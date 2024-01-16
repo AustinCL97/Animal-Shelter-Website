976d007defa5094c3603d9103e7ede98353e9ab9
Kyle Huismann <kkhuismann21@gmail.com>
2024-01-16 15:09:57 -0500

    css

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="capstone-app">
+     <header>
+      <img src="./images/Animal Shelter Wide.jpg" alt="Shelter Puppies">
+    </header>
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
