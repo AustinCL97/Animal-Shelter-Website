e5a082c77e458d1dab1b1102c8b538942a85d5c2
AustinCL97 <austin.lockwood1997@gmail.com>
2024-01-18 10:17:13 -0500

    new photo form

--- vue/src/components/AddPhotoForm.vue
+++ vue/src/components/AddPhotoForm.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div class="photo-form">
     <form>
         <div>
             <label>Photo URL:</label>
-            <input type="text">
+            <input type="text" v-bind="newPhoto.photoUrl">
         </div>
         <div>
             <label>Pet ID</label>
-            <input type="text">
+            <input type="text" v-bind="newPhoto.petId">
         </div>
         <input type="submit">
     </form>
@@ -15,15 +15,34 @@
 </template>
 
 <script>
+import PetService from '../services/PetService';
 export default {
-   return: {
-        data(){
-            newPhoto: {}
-        }
+   data(){
+    return {
+        newPhoto: {}
+    }
+   },
+   methods: {
+    addPhoto(){
+        PetService.addPetPhoto(this.newPhoto).then(
+            (response) => {
+                this.$router.push({ name: "pets" })
+            }
+        )
+
+        
+    }
    }
 }
 </script>
 
 <style>
-
+.photo-form{
+    height: 250px;
+    widows: 400px;
+    border-style: solid;
+    border-width: 2px;
+    border-color: black;
+    border-radius: 5px;
+}
 </style>
